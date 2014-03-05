f6a525c338dac2163961ebb245d14c0f073fb299
Alex Kit <source.kit@gmail.com>
2014-03-05 18:47:44 +0100

    travis - do not build the app

--- test/dom/compos/foo.js
+++ test/dom/compos/foo.js
@@ -1,31 +1,35 @@
 
-mask.registerHandler(':foo', mask.Compo({
-	tagName: 'div',   
-	template: 'input value="~[text]"; button x-signal="mousedown:changeToQux" > "Change to baz"',
-	onRenderStart: function(){
+include
+	.load('foo.mask')
+	.done(function(resp){
 		
-		this.model = {
-			text: 'foo'
-		};
-	},
-	
-	compos: {
-		'test_jQuery': '$: button',
-		'test_querySelector': 'button'
-	},
-	
-	events: {
-		'click: button': function(){
+		mask.registerHandler(':foo', mask.Compo({
+			tagName: 'div',   
+			template: resp.load.foo,
+			onRenderStart: function(){
+				
+				this.model = {
+					text: 'foo'
+				};
+			},
 			
-			this.$.find('input').val('baz');
-		}
-	},
-	
-	slots: {
-		changeToQux: function(event){
+			compos: {
+				'test_jQuery': '$: button',
+				'test_querySelector': 'button'
+			},
 			
-			this.$.find('input').val('qux');
-		}
-	}
-}));
-
+			events: {
+				'click: button': function(){
+					
+					this.$.find('input').val('baz');
+				}
+			},
+			
+			slots: {
+				changeToQux: function(event){
+					
+					this.$.find('input').val('qux');
+				}
+			}
+		}));
+	})
