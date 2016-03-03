c9c9988a07cfb3b5d25d45ccd4f6d652165b53bf
Alex Kit <source.kit@gmail.com>
2016-03-03 16:09:48 +0100

    fix (CtxModels) return empty object when models is null

--- src/builder/ctx/CtxModels.js
+++ src/builder/ctx/CtxModels.js
@@ -1,4 +1,4 @@
-(function(){	
+(function(){
 	builder_CtxModels = class_create({
 		constructor: function (model, startIndex) {
 			this._models = null;
@@ -9,49 +9,46 @@
 			return add(this, model);
 		},
 		tryAppend: function(ctr){
-			
+
 			if (mode_SERVER_ALL === ctr.mode)
 				return -1;
-			
+
 			if (mode_model_NONE === ctr.modeModel)
 				return -1;
-			
+
 			var model = ctr.modelRef != null
 				? '$ref:' + ctr.modelRef
 				: ctr.model
 				;
 			return add(this, model);
 		},
-		
+
 		stringify: function(){
 			return stringify(this._models);
 		}
 	});
-	
+
 	// private
-	
+
 	function add(modelBuilder, model) {
-		if (model == null) 
+		if (model == null)
 			return -1;
 		if (modelBuilder._models == null) {
 			modelBuilder._models = {};
 		}
-		
+
 		var id = 'm' + (++modelBuilder._id);
 		modelBuilder._models[id] = model;
 		return id;
 	}
 	var stringify;
 	(function(){
+		var fn = typeof Class !== 'undefined' && is_Function(Class.stringify)
+			? Class.stringify
+			: JSON.stringify
+			;
 		stringify = function (models) {
-			if (models == null) {
-				return null;
-			}
-			stringify = typeof Class !== 'undefined' && is_Function(Class.stringify)
-				? Class.stringify
-				: JSON.stringify
-				;
-			return stringify(models);
+			return models == null ? '{}' : fn(models);
 		};
 	}())
 }());
