7982c248ccf85beb840db7a27225972b958cecc9
Alex Kit <source.kit@gmail.com>
2016-03-04 10:14:10 +0100

    fix (Bootstrap: utils) pass the type

--- src/client/setup-util.js
+++ src/client/setup-util.js
@@ -9,13 +9,13 @@ function setup_util(meta, node, model, ctx, container, ctr) {
 		console.error('Custom Utility Handler was not defined', meta.name);
 		return node;
 	}
-	
+
 	util = handler.util;
 	el =  meta.utilType === 'attr'
 		? trav_getElement(node)
 		: node.nextSibling
 		;
-	
+
 	if (util === void 0 || util.mode !== 'partial') {
 		handler(
 			meta.value
@@ -28,8 +28,8 @@ function setup_util(meta, node, model, ctx, container, ctr) {
 		);
 		return node;
 	}
-	
-		
+
+
 	util.element = el;
 	util.current = meta.utilType === 'attr'
 		? meta.current
@@ -42,11 +42,12 @@ function setup_util(meta, node, model, ctx, container, ctr) {
 		, el
 		, ctr
 		, meta.attrName
+		, meta.utilType
 	);
-	
+
 	if (meta.utilType === 'node') {
 		node = el.nextSibling;
 	}
-	
+
 	return node;
 }
