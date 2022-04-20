64a8b0832cb5cdb4815bd7df410ae8eb12de6566
Alex Kit <alex.kit@atmajs.com>
2022-04-20 14:37:44 +0200

    update (mask)

--- src/mock/util-handler.ts
+++ src/mock/util-handler.ts
@@ -4,13 +4,13 @@ import { is_Function, is_Object } from '@utils/is';
 import { log_error } from '@core/util/reporters';
 
 const mock_UtilHandler = {
-    create (name, mix, mode) {
+    create (name, mix, mode: 'server' | 'partial') {
         if (mode === 'server')
             return mix;
 
         // partial | client
-        return function (val, model, ctx, el, ctr, attrName, type) {
-            var node = new HtmlDom.UtilNode(type, name, val, attrName /*, ++ctx._id */);
+        return function (val, model, ctx, el, ctr, attrName, type: 'node' | 'attr') {
+            let node = new HtmlDom.UtilNode(type, name, val, attrName /*, ++ctx._id */);
             if (type === 'attr') {
                 el
                     .parentNode
@@ -18,27 +18,26 @@ const mock_UtilHandler = {
             }
 
             if (mode === 'partial') {
-                var fn = util_FNS[type],
-                    current;
-                if (is_Function(mix[fn]) === false) {
-                    log_error('Utils partial function is not defined', fn);
+                let fnName = util_FNS[type];
+                if (is_Function(mix[fnName]) === false) {
+                    log_error('Utils partial function is not defined', fnName);
                     return '';
                 }
 
-                current = mix[fn](val, model, ctx, el, ctr);
+                let currentVal = mix[fnName](val, model, ctx, el, ctr);
                 if (type === 'node') {
                     node.appendChild(mix.element);
                     return node;
                 }
 
                 //> attr
-                return node.meta.current = current;
+                return node.meta.current = currentVal;
             }
 
             /* client-only */
-            if (type === 'node')
+            if (type === 'node') {
                 return node;
-
+            }
             //> attr
             return '';
         };
